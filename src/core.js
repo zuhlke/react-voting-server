6a5c54238426d1b404d1501916a885647a6fec73
Lachezar Kozhuharov <lachezar.kozhuharov@zuhlke.com>
2016-09-06 19:53:41 +0100

    Add implementation for starting a vote

--- src/core.js
+++ src/core.js
@@ -2,3 +2,9 @@ export const setEntries = (state, entries) => ({
   ...state,
   entries: entries
 })
+
+export const next = (state) => ({
+  ...state,
+  vote: {pair: state.entries.slice(0, 2)},
+  entries: state.entries.slice(2)
+})
