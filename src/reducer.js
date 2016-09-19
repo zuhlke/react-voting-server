18b8aa764ca8575a7a050d9514571144e8356392
Lachezar Kozhuharov <lachezar.kozhuharov@zuhlke.com>
2016-09-19 15:28:20 +0200

    Add implementation for reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,12 @@
-const reducer = (state, {type, payload}) => {}
+import {setEntries, next, vote} from './core'
+
+const reducer = (state, {type, payload}) => {
+  switch (type) {
+    case 'SET_ENTRIES': return setEntries(state, payload.entries)
+    case 'NEXT': return next(state)
+    case 'VOTE': return vote(state, payload.entry)
+  }
+  return state
+}
 
 export default reducer
