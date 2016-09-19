4e5fd6db2c19aff09f6f3996a6e12bf16f2203f1
Lachezar Kozhuharov <lachezar.kozhuharov@zuhlke.com>
2016-09-19 20:01:49 +0200

    Add receiving remote actions

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import makeStore from './src/store'
 import startServer from './src/server'
 
 export const store = makeStore()
-startServer()
+startServer(store)
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  payload: {entries: require('./entries.json')}
+})
+store.dispatch({type: 'NEXT'})
--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import Server from 'socket.io'
 
-const startServer = () => {
+const startServer = (store) => {
   const io = new Server().attach(8090)
+
+  store.subscribe(
+    () => io.emit('state', store.getState())
+  )
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState())
+    socket.on('action', store.dispatch.bind(store))
+  })
+  
 }
 
 export default startServer
